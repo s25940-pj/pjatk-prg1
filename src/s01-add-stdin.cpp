224f3886256deb1eb2ce0f2efef5d6e86d6d226d
Maksymilian Mrówka <s25940@pjwstk.edu.pl>
2021-10-23 18:28:28 +0200

    Wykonalem zadania z cwiczen 23/10/21

---
+++ src/s01-add-stdin.cpp
@@ -0,0 +1,23 @@
+# include <iostream>
+
+
+int input;
+
+auto ask_user_for_integer() -> int
+{
+std::cout << "Type in int, please: ";
+std::cin >> input;
+
+return input;  
+}
+
+
+auto main() -> int
+{
+auto const a = ask_user_for_integer();
+auto const b = ask_user_for_integer();
+
+std::cout << (a + b) << "\n";
+
+return 0;
+}
---
+++ src/s01-hello-me.cpp
@@ -0,0 +1,12 @@
+/*
+ * The very first program you should write in any new programming language.
+ */
+
+
+#include <iostream>
+
+auto main() -> int
+{
+    std::cout << "Hello, Maks!\n";
+    return 0;
+}
