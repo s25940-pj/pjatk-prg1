d8e79a6c5692f5d9daf00e989f98eecfb0696b57
Maksymilian Mrówka <s25940@pjwstk.edu.pl>
2021-11-23 13:10:55 +0100

    skompilowalem fizzbuzz i dodalem echo z bledem

--- src/s03-echo.cpp
+++ src/s03-echo.cpp
@@ -3,31 +3,65 @@
 #include <iterator>
 #include <string>
 #include <vector>
+#include <bits/stdc++.h>
+#include <boost/lexical_cast.hpp>
+
+auto i = 1;
 
 
-auto main(int argc, string* argv[]) -> int
+auto format(auto argc_, bool n, bool r, bool l, const std::vector<std::string> to_format) -> int
 {
-    auto args = std::vector<std::string>{};
-    std::copy_n(argv, argc, std::back_inserter(args));
+	auto separator = std::string{};	
 
-    for (auto const& i : args(args.begin() + 1, args.end() -1))
-    {
-        switch (i)
+	if (l == true)
+	{	
+		separator = "\n";
+	}
+	
+	for (; i <= argc_; i++)
+	{	
+		{
+		auto stringed_each = boost::lexical_cast<std::string>(to_format[i]);
+		if (r == true)
+		{	
+			reverse(stringed_each.begin(), stringed_each.end());
+		}
+
+		std::cout << stringed_each << separator;
+		}			
+	}
+
+	if (n == true)
 	{
-	    case "-n":
-	        std::cout << args.back().c;
-	    case "-r":
-	        for (auto i = args.back().length()-1; i >=0; i--)
-		{
-		    cout << args.back().c[i]; 
-		}
-	    case "-l":
-	        for (auto const& i : args.back().c)
-		{
-		    std::cout << i << "\n";
-		}
-	}  
-    }
-         
-    return 0;
+		std::cout << "\n";
+	}
+	
+	return 0;
+	
 }
+
+auto main(int argc, char* argv[]) -> int
+{
+	auto args = std::vector<std::string>{};
+	std::copy_n(argv, argc, std::back_inserter(args));
+	
+	auto new_line = true;
+	auto reverse = false;
+	auto one_for_line = false;
+		
+	for (; i <= argc - 2; i++) // set proper flags
+	{
+		if (strcmp(argv[i], "-n") == 0)
+			new_line = false;
+		if (strcmp(argv[i], "-r") == 0)
+			reverse = true;
+		if (strcmp(argv[i], "-l") == 0)
+			one_for_line = true;
+	}
+
+	auto to_format = args;
+
+	format(argc, new_line, reverse, one_for_line, to_format);
+	return 0;	
+}
+
